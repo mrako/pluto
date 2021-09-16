f50154f7d6ca7504c0416bfb0c6baac7bff7bdb2
Aapo Romu <aapo.romu@eficode.com>
2021-09-16 16:52:29 +0300

    Allow all origins

--- amplify/backend/function/projects/src/index.py
+++ amplify/backend/function/projects/src/index.py
@@ -6,10 +6,10 @@ def handler(event, context):
   
   return {
       'statusCode': 200,
-    #   'headers': {
-    #       'Access-Control-Allow-Headers': '*',
-    #       'Access-Control-Allow-Origin': '*',
-    #       'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-    #   },
+      'headers': {
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      },
       'body': json.dumps('Hello from your new Amplify Python lambda!')
   }
