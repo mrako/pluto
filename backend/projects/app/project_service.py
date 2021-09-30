032f532ee83a1f456f18ac7e11d216ac1fe5aa29
Adrian Miksa <adrian.miksa@eficode.com>
2021-09-30 09:05:37 +0200

    WIP add functions to get repository info and create a repository from template

--- backend/projects/app/project_service.py
+++ backend/projects/app/project_service.py
@@ -1,6 +1,7 @@
 import requests, json
+import pprint
 
-AUTH_TOKEN = "YOUR_GITHUB_TOKEN_HERE"
+AUTH_TOKEN = "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"
 PROJECTS_BASE_URL = "https://api.github.com/orgs/EficodeEntDemo/projects"
 REPOSITORIES_BASE_URL = "https://api.github.com/orgs/EficodeEntDemo/repos"
 
@@ -22,3 +23,19 @@ def add_project_to_github(name, description):
     if resp.status_code != 201:
         raise Exception(f"Failed to create project with response code {resp.status_code}")
 
+
+def get_repository_info(owner, repo):
+    resp = requests.get(f'https://api.github.com/repos/{owner}/{repo}', headers=auth_headers())
+    if resp.status_code != 200:
+        raise Exception(f"Failed to get repository info with response code {resp.status_code}")
+    return resp
+
+
+def create_repository_from_template(owner, template_repo, name, description):
+    resp = requests.post(f'https://api.github.com/repos/{owner}/{template_repo}/generate', headers=auth_headers(), json={'name': name,
+                                                                                                                'owner':owner,
+                                                                                                                'description': description,
+                                                                                                                'private': True})
+    print(resp.json())
+    if resp.status_code != 201:
+        raise Exception(f"Failed to create repository with template: {template_repo} response code {resp.status_code}")
