f160024cbad0ea06fa6c2824d8c77c40601805bf
Eero Nevalainen <eero.nevalainen@eficode.com>
2021-10-07 12:28:57 +0300

    Cleaner organisation project creation

--- backend/endpoints/app/project_service.py
+++ backend/endpoints/app/project_service.py
@@ -2,7 +2,8 @@ import requests
 import logging as log
 from flask import current_app as app
 from uuid import UUID
-from util import query_db, add_error, github_auth_headers, build_error_result
+from api import db
+from util import query_db, github_auth_headers, build_result, build_error_result
 
 from ariadne import convert_kwargs_to_snake_case
 
@@ -54,13 +55,19 @@ def get_project_by_user(*_, user_uuid: UUID, project_uuid: UUID):
 
 @convert_kwargs_to_snake_case
 def add_project_to_github(*_, name: str, description: str):
-    result = query_db('project', insert_project, name=name, description=description)
-    resp = requests.post(app.config['GITHUB_BASE_URL']+"orgs/EficodeEntDemo/projects", headers=github_auth_headers(),
-                         json={'name': name, 'body': description})
-    if resp.status_code != 201:
-        log.warning(f"Failed to create project with response code {resp.status_code}: {resp.text}")
-        add_error(result, "Github project creation failed")
-    return result
+    try:
+        proj = insert_project(name=name,
+                              description=description,
+                              commit_transaction=False)
+        resp = requests.post(f"{app.config['GITHUB_BASE_URL']}orgs/{app.config['GITHUB_ORG_NAME']}/projects",
+                             headers=github_auth_headers(),
+                             json={'name': name, 'body': description})
+        if resp.status_code != 201:
+            raise Exception(f"Failed to create project with response code {resp.status_code}: {resp.text}")
+        return build_result("project", proj)
+    except Exception as e:
+        db.session.rollback()
+        return build_error_result(str(e), e)
 
 
 @convert_kwargs_to_snake_case
