17f6666352b6ea6694e68377d218f1bbe5c643d2
Eero Nevalainen <eero.nevalainen@eficode.com>
2021-10-18 15:00:37 +0300

    Fixed imports to match code style

--- backend/endpoints/app/services/repository_service.py
+++ backend/endpoints/app/services/repository_service.py
@@ -7,22 +7,22 @@ from util import query_db, github_auth_headers, build_result, build_error_result
 
 from ariadne import convert_kwargs_to_snake_case
 
-from dao.repository_dao import find_repository, insert_repository, delete_repository, find_repository_by_url
-from dao.project_dao import insert_project
+import dao.repository_dao as repository_dao
+import dao.project_dao as project_dao
 
 
 @convert_kwargs_to_snake_case
 def get_repository(*_, repository_uuid: UUID):
-    return query_db('repository', find_repository, repository_uuid=repository_uuid)
+    return query_db('repository', repository_dao.find_repository, repository_uuid=repository_uuid)
 
 
 @convert_kwargs_to_snake_case
 def add_repository_to_github(*_, url: str, name: str, description: str):
     try:
-        if find_repository_by_url(url):
+        if repository_dao.find_repository_by_url(url):
             raise Exception("Repository with given url already exists")
 
-        repo = insert_repository(url, name, description, False)
+        repo = repository_dao.insert_repository(url, name, description, False)
         resp = requests.post(f"{app.config['GITHUB_BASE_URL']}orgs/{app.config['GITHUB_ORG_NAME']}/repos",
                              headers=github_auth_headers(),
                              json={'url': url, 'name': name, 'body': description})
@@ -31,10 +31,10 @@ def add_repository_to_github(*_, url: str, name: str, description: str):
             raise Exception("Github repository creation failed")
 
         # Add repository project
-        proj = insert_project(name=f"Repository {repo.name}-project",
-                              description="Auto-generated project for repository",
-                              repository=repo,
-                              commit_transaction=False)
+        proj = project_dao.insert_project(name=f"Repository {repo.name}-project",
+                                          description="Auto-generated project for repository",
+                                          repository=repo,
+                                          commit_transaction=False)
 
         resp = requests.post(f"{app.config['GITHUB_BASE_URL']}repos/{app.config['GITHUB_ORG_NAME']}/"
                              f"{repo.name}/projects",
@@ -53,7 +53,7 @@ def add_repository_to_github(*_, url: str, name: str, description: str):
 
 @convert_kwargs_to_snake_case
 def delete_repository_from_github(*_, repository_uuid: UUID):
-    repo = find_repository(repository_uuid)
+    repo = repository_dao.find_repository(repository_uuid)
     if not repo:
         return {
             'success': False,
@@ -64,10 +64,10 @@ def delete_repository_from_github(*_, repository_uuid: UUID):
                                f"{repo.name}",
                                headers=github_auth_headers())
         if resp.status_code == 204:
-            delete_repository(repo.uuid)
+            repository_dao.delete_repository(repo.uuid)
             return {'success': True, 'errors': []}
         elif resp.status_code == 404:  # Is this case sane to handle like this?
-            delete_repository(repo.uuid)
+            repository_dao.delete_repository(repo.uuid)
             return {'success': True, 'errors': ['Repository was not found on GitHub but was deleted from db']}
         elif resp.status_code == 403:
             raise Exception("Repository deletion not allowed on GitHub")
