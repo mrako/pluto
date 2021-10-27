ea280cb6ecc8dc67947fba15962fbb26b5a89d39
Aapo Romu <aapo.romu@eficode.com>
2021-10-27 14:48:21 +0300

    Mock the Cognito public key query

--- backend/endpoints/tests/test_JWT_common.py
+++ backend/endpoints/tests/test_JWT_common.py
@@ -1,16 +1,56 @@
+from unittest.mock import MagicMock
+
+import requests
 import unittest
 import os
 
 from utils.jwt_common import JWTParser, JWTVerificationException
 
 
+class MockCognitoResponse:
+    def __init__(self, url, **kwargs):
+        # Get url parameter values - applies only for single digits.
+        pass
+
+    def json(self):
+        return {
+              "keys": [
+                {
+                  "alg": "RS256",
+                  "e": "AQAB",
+                  "kid": "pSxliCVEmFQ0XSjyn+Tay/1LGBcHnqPptFrUPx7iQv8=",
+                  "kty": "RSA",
+                  "n": "uhvK43jhSLaQf5PWXNox9wuKh8S5QlAJF2uS72Xht8iArv-C8ouAk0r5u2LXeINOjvguAh65xDEgDMmP4XwsvtrQyJ8-kRpNfEmNXmHmgliORBCqpdnIyAsZqkCc6Oq9dWO66ZHWPW9uZ8BqVbqGzRsm2yGp5VIcrjjmb_Yrcl6-UOYIOGr8P3Mcb39dFGCW_SQKtzO9H0piTIJUupOkIMm1MQ92SbCh9la9asSN2nVIAjvekMQL9iOvOzYiFBvp4hfkLoaiS83C2gIHWMfnolgtXGDOE5PLoYoGBn-LUVqEQX31jX99T5-q2JNN3WzcbFVqrABlWd_osHWEmMMf_w",
+                  "use": "sig"
+                },
+                {
+                  "alg": "RS256",
+                  "e": "AQAB",
+                  "kid": "jh9oJngxBZKyh6Fd0voN5V2xlayksfFKH6Plf9bBkAI=",
+                  "kty": "RSA",
+                  "n": "rPp1_Vkh3QzjrbY_UzzCBII4Nb0DfEUFbv_0CMRNL0x7Yvg1FSBwCbc5ZCVZe6gTE3HdUz508dNbgyaBQwFewS78C8K2bHoadXoqQGgTT1QHLnBknXsgt-xWiDZ3v4JE2e_Las0QBBcYC7imMRG0yPRZYzSN26jyJGdXP7RlxB6U3M6rZDtQ__inArNJBZoqzLO-uCYDD_2kQlduNL6q-T3Z8_EbZgUH59YWOhrGTsV17FlELP8pwxyq745oSxZB23x6IgW-qaqFtPVEzhGWjEvW9Ice_3X94J4hOIqWj7JuP5ycjFk6p4tieyPOV5RjlE1vOHwOKLgItD3yVkaG7Q",
+                  "use": "sig"
+                }
+              ]
+            }
+
+
 class TestJwtCommon(unittest.TestCase):
 
-    def test_parse_expired_access_token(self):
-        os.environ['AWS_REGION'] = 'eu-west-1'
-        os.environ['USER_POOL_ID'] = 'eu-west-1_KAyNhS2iQ'
-        os.environ['APP_CLIENT_ID'] = '56t4dnh8n5v8jkd41f9ssv02mt'
+    @classmethod
+    def setUpClass(cls):
+        region = 'aws-region-in-use'
+        userpool_id = 'aws-user-pool-id-FOang4duh'
+        client_id = '12f6wrg4h5g28jkfofofofofo'
+        os.environ['AWS_REGION'] = region
+        os.environ['USER_POOL_ID'] = userpool_id
+        os.environ['APP_CLIENT_ID'] = client_id
 
+        requests.get = MagicMock(side_effect=MockCognitoResponse)
+        url = f"https://cognito-idp.{region}.amazonaws.com/{userpool_id}/.well-known/jwks.json"
+        response = requests.get(url).json()
+
+    def test_parse_expired_access_token(self):
         try:
             parser = JWTParser()
             parser.parse_token(
