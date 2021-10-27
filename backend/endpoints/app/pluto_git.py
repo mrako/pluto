1ca8fd19e0815c41a0ffb127af56b5c358408d10
Eero Nevalainen <eero.nevalainen@eficode.com>
2021-10-27 08:44:16 +0300

    Refactoring git stuff into own lambda

---
+++ backend/endpoints/app/pluto_git.py
@@ -0,0 +1,30 @@
+import logging as log
+from requests import request
+from api import app, BASE_ROUTE
+import services.template_service as service
+
+BASE_PATH = BASE_ROUTE + 'pluto-git'
+
+
+def handler(event, context):
+    handle_template_service_call(event)
+    return event
+
+
+def handle_template_service_call(event):
+    service.run_template_service(event.get('repo_url'),
+                                 event.get('template'),
+                                 event.get('branch', None))
+
+
+@app.route(BASE_PATH, methods=["POST"])
+def test_endpoint():
+    payload = request.get_json()
+    log.info(f"PAYLOAD {payload}")
+    handle_template_service_call(payload)
+
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=8085, debug=True)
