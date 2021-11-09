8e58c96f50de5a28678c5b63d588379ec6bb8bd5
Mikki Levón <mikki.levon@eficode.com>
2021-11-09 15:02:24 +0200

    remove dataOrigin

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -2,4 +2,4 @@ export function getProjectsQuery(): string { return 'query { projects { success 
 
 export function getProjectByUUIDQuery(uuid:string): string { return `query { project(projectUuid: "${uuid}") { success errors project { name description uuid }}}`; }
 
-export function getUserLinksQuery(): string { return 'query { userLinks { success errors links { uuid organisation { name dataOrigin } projectUser { username dataOrigin } } } }'; }
+export function getUserLinksQuery(): string { return 'query { userLinks { success errors links { uuid organisation { name } projectUser { username } } } }'; }
