dd08da66e3f285545ceddecd57ca705433acbf39
Mikki <mikki.levon@eficode.com>
2021-11-11 10:42:45 +0200

    fix

--- src/types/types.ts
+++ src/types/types.ts
@@ -10,7 +10,15 @@ export interface IProject {
   name: string,
   description?: string,
   uuid: string,
+  repositories?: IRepository[]
 }
 
+export interface IRepository {
+  uuid: string,
+  url: string,
+  name: string,
+  description?: string
+}
 // Infer the `RootState type from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
