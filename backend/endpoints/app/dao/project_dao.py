b870708af5bd11acaea05566a3c7912b9ebce6bc
aromu <aapo.romu@eficode.com>
2021-11-15 10:31:41 +0200

    Enable multiple installations per project user

--- backend/endpoints/app/dao/project_dao.py
+++ backend/endpoints/app/dao/project_dao.py
@@ -1,7 +1,7 @@
 from uuid import UUID, uuid4
 
 from api import db
-from models import Project, Repository, ProjectMember, DataOrigin, ProjectUser, UserLink
+from models import Project, Repository, ProjectMember, DataOrigin, ProjectUser, UserLink, ProjectUserAttribute
 
 
 def find_projects_query():
@@ -85,18 +85,30 @@ def find_user_by_ext_id(data_origin: DataOrigin, ext_id):
 
 def get_user_by_installation_id(installation_id):
     return db.session.query(ProjectUser)\
-        .filter(ProjectUser.installation_id == f"{installation_id}").one()
+        .join(ProjectUserAttribute, ProjectUserAttribute.project_user_uuid == ProjectUser.uuid) \
+        .filter(ProjectUserAttribute.name == 'installation_id') \
+        .filter(ProjectUserAttribute.value == f"{installation_id}")\
+        .one()
 
 
-def create_project_user(data_origin: DataOrigin, installation_id: int, ext_id, username: str):
+def create_project_user(data_origin: DataOrigin, ext_id, username: str):
     uuid = uuid4()
     user = ProjectUser(
         uuid=uuid,
         data_origin_uuid=data_origin.uuid,
         external_id=f"{ext_id}",
-        installation_id=installation_id,
         username=username
     )
     db.session.add(user)
     db.session.flush()
     return user
+
+
+def add_installation_id(user: ProjectUser, installation_id: int):
+    attribute = ProjectUserAttribute(
+        uuid=uuid4(),
+        project_user_uuid=user.uuid,
+        name='installation_id',
+        value=installation_id
+    )
+    db.session.add(attribute)
